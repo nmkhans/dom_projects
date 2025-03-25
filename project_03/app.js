93de05515a121f1a7ca8e5514aa269e30d16972c
nmkhans <nurmoin05@gmail.com>
2025-03-25 19:57:52 +0600

    initial commit

---
+++ project_03/app.js
@@ -0,0 +1,51 @@
+/**
+ * Project requirements:
+ * - Change the background color by generating a random hex color on button click
+ * - Display the hex color on the disabled input field
+ * - Add a button to copy the color code
+ */
+
+// Steps:
+
+// step 1 - create an onload handler
+window.onload = () => {
+  main();
+};
+
+function main() {
+  const root = document.getElementById("root");
+  const output = document.getElementById("output");
+  const changeBtn = document.getElementById("change-btn");
+  const copyBtn = document.getElementById("copy-btn");
+
+  changeBtn.addEventListener("click", () => {
+    const bgColor = generateHexColor();
+    root.style.backgroundColor = bgColor;
+    output.value = bgColor;
+  });
+
+  copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(output.value)
+  })
+}
+
+// step 2 - random hex color generator function
+function generateHexColor() {
+  // #000000, #ffffff
+  // 255, 255, 255 -> ff,ff,ff -> #ffffff
+  // 255 -> ff
+
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(
+    16
+  )}`;
+}
+
+// step 3 - gather necessary references
+
+// step 4 - handle the change button click event
+
+// step 5 - handle the copy button click event
