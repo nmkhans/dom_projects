26b0ea55a3acb3ae8c430c97ac68bbd5b8b7ea63
nmkhans <nurmoin05@gmail.com>
2025-03-26 11:32:07 +0600

    final code pushed to prod

--- production/app.js
+++ production/app.js
@@ -35,17 +35,32 @@ const defaultPresetColors = [
   "#ffcc80",
 ];
 
+let customColors = new Array(12);
+
 const copySound = new Audio("./copy-sound.wav");
 
 // onload handler
 window.onload = () => {
   main();
 
+  updateColorCodeToDom(hexToDecimalColor("DDDEEE"));
+
   // show preset colors
   displayColorBoxes(
     document.getElementById("preset-colors"),
     defaultPresetColors
   );
+
+  const customColorsString = localStorage.getItem("custom-color");
+
+  // load saved custom color
+  if (customColorsString) {
+    customColors = JSON.parse(customColorsString);
+    displayColorBoxes(
+      document.getElementById("custom-colors"),
+      customColors
+    );
+  }
 };
 
 // main function
@@ -65,6 +80,16 @@ function main() {
     "copy-to-clipboard"
   );
   const presetColorParent = document.getElementById("preset-colors");
+  const customColorParent = document.getElementById("custom-colors");
+  const saveToCustomBtn = document.getElementById("save-to-custom");
+  const fileInput = document.getElementById("bg-file-input");
+  const fileInputBtn = document.getElementById("bg-file-input-btn");
+  const fileDeleteBtn = document.getElementById("bg-file-delete-btn");
+  const bgPreview = document.getElementById("background-preview");
+  const bgController = document.getElementById("bg-controller");
+  const bgControllerLead = document.getElementById(
+    "bg-controller-lead"
+  );
 
   generateRandomColorBtn.addEventListener(
     "click",
@@ -102,10 +127,61 @@ function main() {
 
   copyToClipBoardBtn.addEventListener("click", handleCopyToClipBoard);
 
-  presetColorParent.addEventListener("click", handlePresetColorClick);
+  presetColorParent.addEventListener(
+    "click",
+    handlePresetAndCustomColorClick
+  );
+
+  saveToCustomBtn.addEventListener(
+    "click",
+    handleSaveToCustom(hexInput, customColorParent)
+  );
+
+  customColorParent.addEventListener(
+    "click",
+    handlePresetAndCustomColorClick
+  );
+
+  fileInputBtn.addEventListener("click", function () {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener(
+    "change",
+    handleFileChange(
+      bgPreview,
+      fileDeleteBtn,
+      bgController,
+      bgControllerLead
+    )
+  );
+
+  fileDeleteBtn.addEventListener(
+    "click",
+    handleFileDeleteBtnClick(
+      bgPreview,
+      fileDeleteBtn,
+      fileInput,
+      bgController,
+      bgControllerLead
+    )
+  );
+
+  document
+    .getElementById("bg-size")
+    .addEventListener("change", handleChangeBackgroundPreferances);
+  document
+    .getElementById("bg-position")
+    .addEventListener("change", handleChangeBackgroundPreferances);
+  document
+    .getElementById("bg-repeat")
+    .addEventListener("change", handleChangeBackgroundPreferances);
+  document
+    .getElementById("bg-attachment")
+    .addEventListener("change", handleChangeBackgroundPreferances);
 }
 
-// event handlers
+//? event handlers
 
 function handleGenerateRandomColorBtn() {
   const color = generateColorDecimal();
@@ -178,7 +254,7 @@ function handleCopyToClipBoard() {
   }
 }
 
-function handlePresetColorClick(e) {
+function handlePresetAndCustomColorClick(e) {
   const element = e.target;
   if (element.className === "color-box") {
     const hexColor = element.getAttribute("data-color");
@@ -196,6 +272,76 @@ function handlePresetColorClick(e) {
     updateColorCodeToDom(decimalColor);
     generateToastMessage(`${hexColor.toUpperCase()} copied.`);
   }
+}
+
+function handleSaveToCustom(hexInput, customColorParent) {
+  return function () {
+    const hexColor = `#${hexInput.value}`;
+
+    if (customColors.includes(hexColor)) {
+      alert("Color already exist in list");
+      return;
+    }
+    customColors.unshift(hexColor);
+
+    if (customColors.length > 12) {
+      customColors = customColors.slice(0, 12);
+    }
+
+    localStorage.setItem(
+      "custom-color",
+      JSON.stringify(customColors)
+    );
+
+    customColorParent.innerHTML = "";
+    displayColorBoxes(customColorParent, customColors);
+  };
+}
+
+function handleChangeBackgroundPreferances() {
+  document.body.style.backgroundSize =
+    document.getElementById("bg-size").value;
+  document.body.style.backgroundRepeat =
+    document.getElementById("bg-repeat").value;
+  document.body.style.backgroundPosition =
+    document.getElementById("bg-position").value;
+  document.body.style.backgroundAttachment =
+    document.getElementById("bg-attachment").value;
+}
+
+function handleFileChange(
+  bgPreview,
+  fileDeleteBtn,
+  bgController,
+  bgControllerLead
+) {
+  return function (e) {
+    const file = e.target.files[0];
+    const imgUrl = URL.createObjectURL(file);
+
+    bgPreview.style.background = `url(${imgUrl})`;
+    document.body.style.background = `url(${imgUrl})`;
+    fileDeleteBtn.style.display = "inline-block";
+    bgController.style.display = "block";
+    bgControllerLead.style.display = "none";
+  };
+}
+
+function handleFileDeleteBtnClick(
+  bgPreview,
+  fileDeleteBtn,
+  fileInput,
+  bgController,
+  bgControllerLead
+) {
+  return function () {
+    bgPreview.style.background = `#DDDEEE`;
+    document.body.style.background = `#DDDEEE`;
+    fileDeleteBtn.style.display = "none";
+    fileInput.value = null;
+    bgController.style.display = "none";
+    bgControllerLead.style.display = "block";
+  };
 }
 
 // DOM function
@@ -293,8 +439,10 @@ function generateColorBox(color) {
  */
 function displayColorBoxes(parent, colors) {
   colors.forEach((color) => {
-    const colorBox = generateColorBox(color);
-    parent.appendChild(colorBox);
+    if (validHexCode(color?.slice(1))) {
+      const colorBox = generateColorBox(color);
+      parent.appendChild(colorBox);
+    }
   });
 }
 
@@ -365,7 +513,9 @@ function hexToDecimalColor(hex) {
  * @returns {boolean}
  */
 function validHexCode(color) {
-  if (color.length !== 6) return false;
+  if (color?.length !== 6) return false;
 
   return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
+
+// project 15 video start at 10 minute
